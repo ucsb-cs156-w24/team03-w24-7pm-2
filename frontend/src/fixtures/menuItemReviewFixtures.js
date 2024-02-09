c2b568dae580634ea0cb20056985e4f6820dffa7
nguyenliliana <liliananguyen@ucsb.edu>
2024-02-09 00:18:29 -0800

    ln - created menuItemReviewFixtures

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,39 @@
+const menuItemReviewFixtures = {
+   oneMenuItemReview: {
+      "id": 1,
+      "itemId": 1,
+      "reviewerEmail": "email1@email.com",
+      "stars": 1,
+      "dateReviewed": "2024-02-09T07:47:01.623Z",
+      "comments": "comment1"
+   },
+   threeMenuItemReviews: [
+      {
+         "id": 1,
+         "itemId": 1,
+         "reviewerEmail": "email1@email.com",
+         "stars": 1,
+         "dateReviewed": "2024-02-09T07:47:01.623Z",
+         "comments": "comment1"
+      },
+      {
+         "id": 2,
+         "itemId": 2,
+         "reviewerEmail": "email2@email.com",
+         "stars": 1,
+         "dateReviewed": "2024-02-10T07:47:01.623Z",
+         "comments": "comment2"
+      },
+      {
+         "id": 3,
+         "itemId": 3,
+         "reviewerEmail": "email3@email.com",
+         "stars": 3,
+         "dateReviewed": "2024-02-11T07:47:01.623Z",
+         "comments": "comment3"
+      }
+   ]
+};
+
+
+export { menuItemReviewFixtures };
