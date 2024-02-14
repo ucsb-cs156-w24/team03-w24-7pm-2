8e33d6c6c0f82d0c0bc9462269d378b3a2bb6408
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2024-02-14 12:24:02 -0800

    AG - added Form and Form.stories.js files

---
+++ frontend/src/stories/components/RecommendationRequests/RecommendationRequestForm.stories.js
@@ -0,0 +1,33 @@
+
+export default {
+    title: 'components/RecommendationRequests/RecommendationRequestForm',
+    component: RecommendationRequestForm
+};
+
+
+const Template = (args) => {
+    return (
+        <RecommendationRequestForm {...args} />
+    )
+};
+
+export const Create = Template.bind({});
+
+Create.args = {
+    buttonLabel: "Create",
+    submitAction: (data) => {
+        console.log("Submit was clicked with data: ", data); 
+        window.alert("Submit was clicked with data: " + JSON.stringify(data));
+   }
+};
+
+export const Update = Template.bind({});
+
+Update.args = {
+    initialContents: recommendationRequestFixtures.oneDate,
+    buttonLabel: "Update",
+    submitAction: (data) => {
+        console.log("Submit was clicked with data: ", data); 
+        window.alert("Submit was clicked with data: " + JSON.stringify(data));
+   }
+};
