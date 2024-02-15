53e7bf9450abc53dd0aae5e5258d43b25a79cef4
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2024-02-15 12:11:47 -0800

    cahnged fixtures index from 1-3 to 2-4

--- frontend/src/fixtures/recommendationRequestFixtures.js
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -10,30 +10,30 @@ const recommendationRequestFixtures = {
     },
     threeRecommendationRequests: [
         {
-            "id": 1,
+            "id": 2,
             "requesterEmail": "requester1@ucsb.edu",
             "professorEmail": "professor1@ucsb.edu",
             "explanation": "explanation1",
-            "dateRequested": "2022-01-02T12:00:00",
-            "dateNeeded": "2022-01-02T12:00:00",
+            "dateRequested": "2022-02-02T12:00:00",
+            "dateNeeded": "2022-02-02T12:00:00",
             "done": "false"
         },
         {
-            "id": 2,
+            "id": 3,
             "requesterEmail": "requester2@ucsb.edu",
             "professorEmail": "professor2@ucsb.edu",
             "explanation": "explanation2",
-            "dateRequested": "2022-02-02T12:00:00",
-            "dateNeeded": "2022-02-02T12:00:00",
+            "dateRequested": "2022-03-02T12:00:00",
+            "dateNeeded": "2022-03-02T12:00:00",
             "done": "true"
         },
         {
-            "id": 3,
-            "requesterEmail": "requester3@ucsb.edu",
-            "professorEmail": "professor3@ucsb.edu",
-            "explanation": "explanation3",
-            "dateRequested": "2022-03-02T12:00:00",
-            "dateNeeded": "2022-03-02T12:00:00",
+            "id": 4,
+            "requesterEmail": "requester2@ucsb.edu",
+            "professorEmail": "professor4@ucsb.edu",
+            "explanation": "explanation4",
+            "dateRequested": "2022-03-03T12:00:00",
+            "dateNeeded": "2022-03-03T12:00:00",
             "done": "false"
         },
     ]
