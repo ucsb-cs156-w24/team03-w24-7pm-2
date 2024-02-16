132ef8a53e2b06ff30f32e7f05f0259f65af4250
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2024-02-16 01:21:24 -0800

    redid stories

--- frontend/src/stories/pages/RecommendationRequests/RecommendationRequestIndexPage.stories.js
+++ frontend/src/stories/pages/RecommendationRequests/RecommendationRequestIndexPage.stories.js
@@ -6,60 +6,59 @@ import { rest } from "msw";
 import RecommendationRequestIndexPage from "main/pages/RecommendationRequests/RecommendationRequestIndexPage";
 
 export default {
-   title: 'pages/RecommendationRequests/recommendationRequestIndexPage',
-   component: RecommendationRequestIndexPage
+    title: 'pages/RecommendationRequests/RecommendationRequestIndexPage',
+    component: RecommendationRequestIndexPage
 };
 
-
-const Template = () => <RecommendationRequestIndexPage storybook={true} />;
+const Template = () => <RecommendationRequestIndexPage storybook={true}/>;
 
 export const Empty = Template.bind({});
 Empty.parameters = {
-   msw: [
-      rest.get('/api/currentUser', (_req, res, ctx) => {
-         return res(ctx.json(apiCurrentUserFixtures.userOnly));
-      }),
-      rest.get('/api/systemInfo', (_req, res, ctx) => {
-         return res(ctx.json(systemInfoFixtures.showingNeither));
-      }),
-      rest.get('/api/recommendationrequests/all', (_req, res, ctx) => {
-         return res(ctx.json([]));
-      }),
-   ]
+    msw: [
+        rest.get('/api/currentUser', (_req, res, ctx) => {
+            return res(ctx.json(apiCurrentUserFixtures.userOnly));
+        }),
+        rest.get('/api/systemInfo', (_req, res, ctx) => {
+            return res(ctx.json(systemInfoFixtures.showingNeither));
+        }),
+        rest.get('/api/recommendationrequests/all', (_req, res, ctx) => {
+            return res(ctx.json([]));
+        }),
+    ]
 }
 
 export const ThreeItemsOrdinaryUser = Template.bind({});
 
 ThreeItemsOrdinaryUser.parameters = {
-   msw: [
-      rest.get('/api/currentUser', (_req, res, ctx) => {
-         return res(ctx.json(apiCurrentUserFixtures.userOnly));
-      }),
-      rest.get('/api/systemInfo', (_req, res, ctx) => {
-         return res(ctx.json(systemInfoFixtures.showingNeither));
-      }),
-      rest.get('/api/recommendationrequests/all', (_req, res, ctx) => {
-         return res(ctx.json(recommendationRequestFixtures.threeRecommendationRequests));
-      }),
-   ],
+    msw: [
+        rest.get('/api/currentUser', (_req, res, ctx) => {
+            return res( ctx.json(apiCurrentUserFixtures.userOnly));
+        }),
+        rest.get('/api/systemInfo', (_req, res, ctx) => {
+            return res(ctx.json(systemInfoFixtures.showingNeither));
+        }),
+        rest.get('/api/recommendationrequests/all', (_req, res, ctx) => {
+            return res(ctx.json(recommendationRequestFixtures.threeRecommendationRequests));
+        }),
+    ],
 }
 
 export const ThreeItemsAdminUser = Template.bind({});
 
 ThreeItemsAdminUser.parameters = {
-   msw: [
-      rest.get('/api/currentUser', (_req, res, ctx) => {
-         return res(ctx.json(apiCurrentUserFixtures.adminUser));
-      }),
-      rest.get('/api/systemInfo', (_req, res, ctx) => {
-         return res(ctx.json(systemInfoFixtures.showingNeither));
-      }),
-      rest.get('/api/recommendationrequests/all', (_req, res, ctx) => {
-         return res(ctx.json(recommendationRequestFixtures.threeRecommendationRequests));
-      }),
-      rest.delete('/api/recommendationrequests', (req, res, ctx) => {
-         window.alert("DELETE: " + JSON.stringify(req.url));
-         return res(ctx.status(200), ctx.json({}));
-      }),
-   ],
+    msw: [
+        rest.get('/api/currentUser', (_req, res, ctx) => {
+            return res( ctx.json(apiCurrentUserFixtures.adminUser));
+        }),
+        rest.get('/api/systemInfo', (_req, res, ctx) => {
+            return res(ctx.json(systemInfoFixtures.showingNeither));
+        }),
+        rest.get('/api/recommendationrequests/all', (_req, res, ctx) => {
+            return res(ctx.json(recommendationRequestFixtures.threeRecommendationRequests));
+        }),
+        rest.delete('/api/recommendationrequests', (req, res, ctx) => {
+            window.alert("DELETE: " + JSON.stringify(req.url));
+            return res(ctx.status(200),ctx.json({}));
+        }),
+    ],
 }
