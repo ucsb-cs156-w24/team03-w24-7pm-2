39b9e1ba4985db08539d70399480e70e71167dca
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2024-02-16 02:43:11 -0800

    added files for editpage reqrequest

--- frontend/src/main/pages/RecommendationRequests/RecommendationRequestEditPage.js
+++ frontend/src/main/pages/RecommendationRequests/RecommendationRequestEditPage.js
@@ -1,13 +1,71 @@
+import RecommendationRequestForm from 'main/components/RecommendationRequests/RecommendationRequestForm';
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { Navigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
-export default function RecommendationRequestEditPage() {
+export default function RecommendationRequestEditPage({storybook=false}) {
+    let { id } = useParams();
 
-  // Stryker disable all : placeholder for future implementation
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
-      </div>
-    </BasicLayout>
-  )
+    const { data: restaurant, _error, _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            [`/api/recommendationrequests?id=${id}`],
+            {  // Stryker disable next-line all : GET is the default, so mutating this to "" doesn't introduce a bug
+                method: "GET",
+                url: `/api/recommendationrequests`,
+                params: {
+                    id
+                }
+            }
+        );
+
+    const objectToAxiosPutParams = (recommendationRequests) => ({
+        url: "/api/recommendationrequests",
+        method: "PUT",
+        params: {
+            id: recommendationRequests.id,
+        },
+        data: {
+            requesterEmail: recommendationRequests.requesterEmail,
+          professorEmail: recommendationRequests.professorEmail,
+          explanation: recommendationRequests.explanation,
+          dateRequested: recommendationRequests.dateRequested,
+          dateNeeded: recommendationRequests.dateNeeded,
+          done: recommendationRequests.done
+        }
+    });
+
+    const onSuccess = (recommendationRequests) => {
+        toast(`RecommendationRequests Updated - id: ${recommendationRequests.id} requesterEmail: ${recommendationRequests.requesterEmail}`);
+    }
+
+    const mutation = useBackendMutation(
+        objectToAxiosPutParams,
+        { onSuccess },
+        // Stryker disable next-line all : hard to set up test for caching
+        [`/api/recommendationrequests?id=${id}`]
+    );
+
+    const { isSuccess } = mutation
+
+    const onSubmit = async (data) => {
+        mutation.mutate(data);
+    }
+
+    if (isSuccess && !storybook) {
+        return <Navigate to="/recommendationrequests" />
+    }
+
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                <h1>Edit RecommendationRequests</h1>
+                {
+                    restaurant && <RecommendationRequestForm submitAction={onSubmit} buttonLabel={"Update"} initialContents={recommendationRequests} />
+                }
+            </div>
+        </BasicLayout>
+    )
+
 }
--- frontend/src/tests/pages/RecommendationRequests/RecommendationRequestEditPage.test.js
+++ frontend/src/tests/pages/RecommendationRequests/RecommendationRequestEditPage.test.js
@@ -1,5 +1,4 @@
-import { render, screen } from "@testing-library/react";
-import RecommendationRequestEditPage from "main/pages/RecommendationRequests/RecommendationRequestEditPage";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
@@ -7,36 +6,212 @@ import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import mockConsole from "jest-mock-console";
 
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        useParams: () => ({
+            id: 17
+        }),
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
 
 describe("RecommendationRequestEditPage tests", () => {
 
-    const axiosMock = new AxiosMockAdapter(axios);
+    describe("when the backend doesn't return data", () => {
 
-    const setupUserOnly = () => {
-        axiosMock.reset();
-        axiosMock.resetHistory();
-        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    };
+        const axiosMock = new AxiosMockAdapter(axios);
 
-    const queryClient = new QueryClient();
-    test("Renders expected content", () => {
-        // arrange
+        beforeEach(() => {
+            axiosMock.reset();
+            axiosMock.resetHistory();
+            axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+            axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+            axiosMock.onGet("/api/restaurants", { params: { id: 17 } }).timeout();
+        });
 
-        setupUserOnly();
+        const queryClient = new QueryClient();
+        test("renders header but table is not present", async () => {
 
-        // act
-        render(
-            <QueryClientProvider client={queryClient}>
-                <MemoryRouter>
-                    <RecommendationRequestEditPage />
-                </MemoryRouter>
-            </QueryClientProvider>
-        );
+            const restoreConsole = mockConsole();
 
-        // assert
-        expect(screen.getByText("Edit page not yet implemented")).toBeInTheDocument();
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <RecommendationRequestEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+            await screen.findByText("Edit RecommendationRequest");
+            expect(screen.queryByTestId("RecommendationRequest-requesterEmail")).not.toBeInTheDocument();
+            restoreConsole();
+        });
     });
 
+    describe("tests where backend is working normally", () => {
+
+        const axiosMock = new AxiosMockAdapter(axios);
+
+        beforeEach(() => {
+            axiosMock.reset();
+            axiosMock.resetHistory();
+            axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+            axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+            axiosMock.onGet("/api/recommendationrequests", { params: { id: 17 } }).reply(200, {
+                id: 17,
+                requesterEmail: "requester17@ucsb.edu",
+                professorEmail: "professor17@ucsb.edu",
+                explanation: "explain17",
+                dateRequested: "2022-03-17T00:00:00",
+                dateNeeded: "2022-03-17T00:00:00",
+                done: "false"
+            });
+            axiosMock.onPut('/api/restaurants').reply(200, {
+                id: 17,
+                requesterEmail: "requester@ucsb.edu",
+                professorEmail: "professor@ucsb.edu",
+                explanation: "explain",
+                dateRequested: "2022-03-01T00:00:00",
+                dateNeeded: "2022-03-01T00:00:00",
+                done: "true"
+            });
+        });
+
+        const queryClient = new QueryClient();
+    
+        test("Is populated with the data provided", async () => {
+
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <RecommendationRequestEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+
+            await screen.findByTestId("RecommendationRequestForm-id");
+
+            const idField = screen.getByTestId("RecommendationRequestForm-id");
+            const requesterEmailField = screen.getByTestId("RecommendationRequestForm-requesterEmail");
+            const professorEmailField = screen.getByTestId("RecommendationRequestForm-professorEmail");
+            const explanationField = screen.getByTestId("RecommendationRequestForm-explanation");
+            const dateRequestedField = screen.getByTestId("RecommendationRequestForm-dateRequested");
+            const dateNeededField = screen.getByTestId("RecommendationRequestFomr-dateNeeded");
+            const doneField = screen.getByTestId("RecommendationRequestForm-done");
+            const submitButton = screen.getByTestId("RestaurantForm-submit");
+
+            expect(idField).toBeInTheDocument();
+            expect(idField).toHaveValue("17");
+            expect( requesterEmailField).toBeInTheDocument();
+            expect(requesterEmailField).toHaveValue("requester17@ucsb.edu");
+            expect(professorEmailField).toBeInTheDocument();
+            expect(professorEmailField).toHaveValue("professor17@ucsb.edu");
+            expect(professorEmailField).toBeInTheDocument();
+            expect(explanationField).toHaveValue("explain17");
+            expect(dateRequestedField).toBeInTheDocument();
+            expect(dateRequestedField).toHaveValue("2022-03-17T00:00:00");
+            expect(dateNeededField).toBeInTheDocument();
+            expect(dateNeededField).toHaveValue("2022-03-17T00:00:00");
+            expect(doneField).toBeInTheDocument();
+            expect(doneField).toHaveValue("false");
+
+            expect(submitButton).toHaveTextContent("Update");
+
+            fireEvent.change(requesterEmailField, { target: { value: 'requester@ucsb.edu' } });
+            fireEvent.change(professorEmailField, { target: { value: 'professor@ucsb.edu' } });
+            fireEvent.change(explanationField, { target: { value: 'explain' } });
+            fireEvent.change(dateRequestedField, { target: { value: '2022-03-01T00:00:00' } });
+            fireEvent.change(dateNeededField, { target: { value: '2022-03-01T00:00:00' } });
+            fireEvent.change(doneField, { target: { value: 'true' } });
+
+            fireEvent.click(submitButton);
+
+            await waitFor(() => expect(mockToast).toBeCalled());
+            expect(mockToast).toBeCalledWith("RecommendationRequests Updated - id: 17 requesterEmail: requester@ucsb.edu");
+            
+            expect(mockNavigate).toBeCalledWith({ "to": "/recommendationrequests" });
+
+            expect(axiosMock.history.put.length).toBe(1); // times called
+            expect(axiosMock.history.put[0].params).toEqual({ id: 17 });
+            expect(axiosMock.history.put[0].data).toBe(JSON.stringify({
+                id: 17,
+                requesterEmail: "requester@ucsb.edu",
+                professorEmail: "professor@ucsb.edu",
+                explanation: "explain",
+                dateRequested: "2022-03-01T00:00:00",
+                dateNeeded: "2022-03-01T00:00:00",
+                done: "true"
+            })); // posted object
+
+
+        });
+
+        test("Changes when you click Update", async () => {
+
+            render(
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter>
+                        <RecommendationRequestEditPage />
+                    </MemoryRouter>
+                </QueryClientProvider>
+            );
+
+            await screen.findByTestId("RestaurantForm-id");
+
+            const idField = screen.getByTestId("RecommendationRequestForm-id");
+            const requesterEmailField = screen.getByTestId("RecommendationRequestForm-requesterEmail");
+            const professorEmailField = screen.getByTestId("RecommendationRequestForm-professorEmail");
+            const explanationField = screen.getByTestId("RecommendationRequestForm-explanation");
+            const dateRequestedField = screen.getByTestId("RecommendationRequestForm-dateRequested");
+            const dateNeededField = screen.getByTestId("RecommendationRequestFomr-dateNeeded");
+            const doneField = screen.getByTestId("RecommendationRequestForm-done");
+            const submitButton = screen.getByTestId("RestaurantForm-submit");
+
+            expect(idField).toBeInTheDocument();
+            expect(idField).toHaveValue("17");
+            expect( requesterEmailField).toBeInTheDocument();
+            expect(requesterEmailField).toHaveValue("requester17@ucsb.edu");
+            expect(professorEmailField).toBeInTheDocument();
+            expect(professorEmailField).toHaveValue("professor17@ucsb.edu");
+            expect(professorEmailField).toBeInTheDocument();
+            expect(explanationField).toHaveValue("explain17");
+            expect(dateRequestedField).toBeInTheDocument();
+            expect(dateRequestedField).toHaveValue("2022-03-17T00:00:00");
+            expect(dateNeededField).toBeInTheDocument();
+            expect(dateNeededField).toHaveValue("2022-03-17T00:00:00");
+            expect(doneField).toBeInTheDocument();
+            expect(doneField).toHaveValue("false");
+            expect(submitButton).toBeInTheDocument();
+
+            fireEvent.change(requesterEmailField, { target: { value: 'requester@ucsb.edu' } });
+            fireEvent.change(professorEmailField, { target: { value: 'professor@ucsb.edu' } });
+            fireEvent.change(explanationField, { target: { value: 'explain' } });
+            fireEvent.change(dateRequestedField, { target: { value: '2022-03-01T00:00:00' } });
+            fireEvent.change(dateNeededField, { target: { value: '2022-03-01T00:00:00' } });
+            fireEvent.change(doneField, { target: { value: 'true' } });
+
+            fireEvent.click(submitButton);
+
+            await waitFor(() => expect(mockToast).toBeCalled());
+            expect(mockToast).toBeCalledWith("RecommendationRequests Updated - id: 17 requesterEmail: requester@ucsb.edu");
+            expect(mockNavigate).toBeCalledWith({ "to": "/recommendationrequests" });
+        });
+
+       
+    });
 });
