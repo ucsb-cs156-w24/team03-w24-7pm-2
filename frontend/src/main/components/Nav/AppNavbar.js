020e10c75a2eeb3b90da7b054b6692c8c0ce3048
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2024-02-15 20:58:47 -0800

    updated link in navbar

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -1,7 +1,7 @@
+import AppNavbarLocalhost from "main/components/Nav/AppNavbarLocalhost";
+import { hasRole } from "main/utils/currentUser";
 import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { hasRole } from "main/utils/currentUser";
-import AppNavbarLocalhost from "main/components/Nav/AppNavbarLocalhost"
 
 export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUrl = window.location.href }) {
   return (
@@ -59,6 +59,7 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                   <Nav.Link as={Link} to="/ucsbdates">UCSB Dates</Nav.Link>
                   <Nav.Link as={Link} to="/menuitemreview">Menu Item Reviews</Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">Placeholder</Nav.Link>
+                  <Nav.Link as={Link} to="/recommendationrequests">RecommendationRequests</Nav.Link>
                 </>
               )
             }
